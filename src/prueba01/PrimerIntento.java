b957c889480c0b892c74a1c3eb680dfec9bfe214
Maldred <Maldred@Ruiditos>
2017-03-30 20:48:31 +0200

    5 version

--- src/prueba01/PrimerIntento.java
+++ src/prueba01/PrimerIntento.java
@@ -5,7 +5,8 @@ public class PrimerIntento {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola Git 3");
+		System.out.println("Hola Git");
+		System.out.println("Funciona de una vez!");
 	}
 
 }
